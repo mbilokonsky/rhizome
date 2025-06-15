d5cc436bcb57672a8d0022e9d522f867df1e7362
Lentil Hoffman <lentil@laddhoffman.com>
2025-06-15 12:32:26 -0500

    chore: remove unused _RhizomeImports from test files
    - Removed unused _RhizomeImports from:
      - __tests__/compose-decompose.ts
      - __tests__/transactions.ts
      - __tests__/negation.ts
    - All tests continue to pass after removal

--- __tests__/negation.ts
+++ __tests__/negation.ts
@@ -1,4 +1,3 @@
-import * as _RhizomeImports from "../src";
 import { Delta } from '../src/core';
 import { NegationHelper } from '../src/features';
 import { RhizomeNode } from '../src/node';
--- __tests__/transactions.ts
+++ __tests__/transactions.ts
@@ -1,4 +1,3 @@
-import * as _RhizomeImports from "../src";
 import { Delta } from '../src/core';
 import { Lossless } from '../src/views';
 import { RhizomeNode } from '../src/node';
