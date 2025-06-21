0b58c4cd0e9d0e20e2f9a0399481bd0daf4d6685
Lentil Hoffman <lentil@laddhoffman.com>
2025-06-21 18:42:49 -0500

    Minor adjustments to test setup

--- __tests__/jest-setup.ts
+++ __tests__/jest-setup.ts
@@ -7,22 +7,9 @@ if (!process.env.DEBUG && !process.env.NO_DEBUG) {
   process.env.DEBUG = 'rz:*';
 }
 
-// Extend the global Jest namespace
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toBeWithinRange(a: number, b: number): R;
-    }
-  }
-}
-
-// Add any global test setup here
-
 // This is a placeholder test to satisfy Jest's requirement for at least one test
 describe('Test Setup', () => {
   it('should pass', () => {
     expect(true).toBe(true);
   });
 });
-
-export {}; // This file needs to be a module
